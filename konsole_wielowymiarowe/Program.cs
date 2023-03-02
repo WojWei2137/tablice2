4074d74c0abb92ffc9f56bcdb103f23fef68588a
WojWei2137 <jan.weiss2137@gmail.com>
2023-03-02 10:30:44 +0100

    tablice wielowymiarowe

---
+++ konsole_wielowymiarowe/Program.cs
@@ -0,0 +1,37 @@
+﻿using Program;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tablica wielowymiarowa
+            int[,] tab = new int[2, 4];
+            Console.WriteLine(tab.Length);  //8
+            Console.WriteLine(tab.GetLength(0));    //2
+            Console.WriteLine(tab.GetLength(1));    //4
+
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    Console.Write($"tab[{i},{j}] ");
+                }
+                Console.WriteLine();
+            }
+
+            Person[] persons = new Person[2];
+            Person[] p1 = new Person("Janusz");
+            Person[] p2 = new Person("Anna");
+
+            persons[0] = p1;
+            persons[1] = p2;
+
+            foreach (Person p in persons)
+            {
+                Console.WriteLine($"imię: {p.firstname} ");
+            }
+        }
+    }
+}
---
+++ konsole_wielowymiarowe/class/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Person
+    {
+        public string firstname = "";
+        public Person(string _firstname) { 
+
+            firstname= _firstname;
+
+        }
+    }
+}
